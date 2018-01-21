c0c3186c177912485de331d358e155922d6ead5a
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-01-21 14:21:06 +0530

    Create countl.c

---
+++ countl.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char a[20];
+    int i,count=1,c;
+    printf("enter the string");
+    gets(a);
+    c=strlen(a);
+    if(a[i]==' ')
+    {
+        count=count+1;
+    }
+    printf("the total words is %d",count);
+getch();
+return o;
+}
