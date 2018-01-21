06a7cf245165d14c3f8b257ef6fb08bcf6bfa456
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-01-21 15:10:18 +0530

    Create ap.java

---
+++ ap.java
@@ -0,0 +1,18 @@
+package guviprogram;
+
+public class ab {
+	String s1="hello";{
+	for( int i=0;i<=s1.length();i++)
+	{
+
+	    char e=s1.charAt(i);
+	    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+	        {
+	String h=s1.substring(0, i);
+
+	String g=s1.substring(i+ i);
+	s1=h+"Z"+g;
+	}}    
+	}
+	    System.out.println("The result is="+s1);                
+}
